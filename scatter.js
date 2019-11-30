93e9d277d5ec7d2bc7446b424b9ad67f264ad3cb
Charles Low <cjlow@andrew.cmu.edu>
2019-11-29 22:50:47 -0500

    First chart

--- scatter.js
+++ scatter.js
@@ -1,17 +1,78 @@
-var trace1 = {
-  x: [1, 2, 3, 4],
-  y: [10, 15, 13, 17],
-  type: 'scatter',
+function makeplot() {
+  Plotly.d3.csv("final/player_scores.csv", function(data){ processData(data) } );
 };
 
-var trace2 = {
-  x: [1, 2, 3, 4],
-  y: [16, 5, 11, 9],
-  type: 'scatter'
+
+function processData(allRows) {
+  var x = [], y = [], player = [];
+  for (var i=0; i<allRows.length; i++) {
+    row = allRows[i];
+    x.push(row['losses']);
+    y.push(row['wins']);
+    player.push(row['player']);
+  }
+  makePlotly(x, y, player);
 };
 
-var data = [trace1, trace2];
 
-Plotly.newPlot('myDiv', data, {}, {showSendToCloud: true});
+function makePlotly(x, y, player) {
+  var traces = [{
+    x: x,
+    y: y,
+    text: player,
+    mode: 'markers',
+    type: 'scatter',
+    hovertemplate: '%{text}<br>' +
+	           '%{y} wins, %{x} losses' +
+	           '<extra></extra>'
+  }];
+
+	
+  var layout = {
+    title: 'Player Wins vs Losses',
+    xaxis: {
+      title: 'Losses',
+      showgrid: false,
+      zeroline: false
+    },
+    yaxis: {
+      title: 'Wins',
+      showline: false,
+      zeroline: false
+    },
+    width: 620,
+    height: 500,
+    margin: {
+      l: 60,
+      r: 0,
+      b: 60,
+      t: 80,
+      pad: 0
+    },
+    hovermode: 'closest'
+  };
+
+  var config = {
+    displayModeBar: false
+  }
+
+  // Make the plot
+  Plotly.newPlot('player_scores', traces, layout, config);
+
+  // Add Hover info to plot
+  //var myPlot = document.getElementById('player_scores');
+  //var hoverInfo = document.getElementById('hoverinfo');
+  //myPlot.on('plotly_hover', function(data) {
+  //  var infotext = data.points.map(function(d) {
+  //    return (d.data.name+': x= '+d.x+', y= '+d.y.toPrecision(3));
+  //  });
+  //    hoverInfo.innerHTML = infotext.join('<br/>');
+  //  })
+  // .on('plotly_unhover', function(data){
+  //    hoverInfo.innerHTML = '';
+  //});
 
 
+};
+
+makeplot();
